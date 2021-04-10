f5f1922aea10b932550e426a3536dbb38b521b31
iiikkk <352453347@qq.com>
2021-04-10 17:11:44 +0800

    2021-4-10 17:11:41

---
+++ src/LeetCode_JAVA/DFS_BFS/SolveSudoku.java
@@ -0,0 +1,66 @@
+package LeetCode_JAVA.DFS_BFS;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode-cn.com/problems/sudoku-solver/
+ * 编写一个程序，通过填充空格来解决9x9数独问题。
+ */
+public class SolveSudoku {
+
+    public static void main(String[] args) {
+        char[][] board = new char[][]{
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+        new SolveSudoku().solveSudoku(board);
+        for (char[] line : board) {
+            System.out.println(Arrays.toString(line));
+        }
+    }
+
+    public void solveSudoku(char[][] board) {
+        dfs(board);
+    }
+
+    private boolean dfs(char[][] board) {
+        for (int i = 0; i < board.length; i++) {        // 枚举行
+            for (int j = 0; j < board[i].length; j++) { // 枚举列
+                if (board[i][j] != '.') continue;
+                for (char k = '1'; k <= '9'; k++) {     // 枚举可以放入的数字
+                    if (!isValid(i, j, k, board)) continue;
+                    board[i][j] = k;
+                    if (dfs(board)) return true; // 如果找到合适一组立刻返回(级联返回)
+                    board[i][j] = '.';
+                }
+                return false;  // 九个数均不成立，无解直接返回
+            }
+        }
+        return true; // 没有返回，肯定有解
+    }
+
+    private boolean isValid(int x, int y, char val, char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][y] == val || board[x][i] == val){
+                return false;
+            }
+        }
+        int startX = (x / 3) * 3;
+        int startY = (y / 3) * 3;
+        for (int i = startX; i < startX + 3 ; i++) {
+            for (int j = startY; j < startY + 3; j++) {
+                if (board[i][j] == val){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/LeetCode_JAVA/Greedy_Algorithm/FindContentChildren.java
@@ -0,0 +1,31 @@
+package LeetCode_JAVA.Greedy_Algorithm;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode-cn.com/problems/assign-cookies/
+ * 假设你是一位很棒的家长，想要给你的孩子们一些小饼干。但是，每个孩子最多只能给一块饼干。
+ * 对每个孩子 i，都有一个胃口值 g[i]，这是能让孩子们满足胃口的饼干的最小尺寸；并且每块饼干 j，都有一个尺寸 s[j] 。
+ * 如果 s[j] >= g[i]，我们可以将这个饼干 j 分配给孩子 i ，这个孩子会得到满足。你的目标是尽可能满足越多数量的孩子，并输出这个最大数值。
+ */
+public class FindContentChildren {
+
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int index = s.length - 1;
+        int result = 0;
+        for (int i = g.length - 1; i >= 0; i--) {
+            if (index >= 0 && s[index] >= g[i]){
+                index--;
+                result++;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] g = {1, 2}, s = {1, 2, 3};
+        System.out.println(new FindContentChildren().findContentChildren(g, s));
+    }
+}
