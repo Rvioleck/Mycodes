e814aef8580d33cae948a9703c7d5860515d21a2
iiikkk <352453347@qq.com>
2021-04-13 19:24:27 +0800

    2021-4-13 19:24:19

--- src/LeetCode_JAVA/DynamicProgram/KnapSackProblem/CompleteKnapSack/SimilarToCKS/NumSquares.java
+++ src/LeetCode_JAVA/DynamicProgram/KnapSackProblem/CompleteKnapSack/SimilarToCKS/NumSquares.java
@@ -1,11 +1,22 @@
 package LeetCode_JAVA.DynamicProgram.KnapSackProblem.CompleteKnapSack.SimilarToCKS;
 
+/**
+ * https://leetcode-cn.com/problems/perfect-squares/
+ * 给定正整数 n，找到若干个完全平方数（比如 1, 4, 9, 16, ...）使得它们的和等于 n。你需要让组成和的完全平方数的个数最少。
+ * 给你一个整数 n ，返回和为 n 的完全平方数的 最少数量 。
+ * 完全平方数 是一个整数，其值等于另一个整数的平方；换句话说，其值等于一个整数自乘的积。例如，1、4、9 和 16 都是完全平方数，而 3 和 11 不是。
+ *
+ * dp[i]：组成数字i的完全平方数的和的个数
+ * dp[i] = min(dp[i - 1*1], dp[i - 2*2], ..., dp[i - j*j])  其中i >= j*j
+ * 初始条件 dp[i] = i 初始化数字i由i个1组成，则组成和个数为i
+ */
+
 public class NumSquares {
 
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            dp[i] = i;
+            dp[i] = i; // 初始化最大值，此时都由1组成时
             for (int j = 1; j <= Math.pow(i, 0.5); j++) {
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
---
+++ src/LeetCode_JAVA/DynamicProgram/KnapSackProblem/CompleteKnapSack/WaysToChange.java
@@ -0,0 +1,43 @@
+package LeetCode_JAVA.DynamicProgram.KnapSackProblem.CompleteKnapSack;
+
+/**
+ * https://leetcode-cn.com/problems/coin-lcci/
+ * 硬币。给定数量不限的硬币，币值为25分、10分、5分和1分，编写代码计算n分有几种表示法。(结果可能会很大，你需要将结果模上1000000007)
+ */
+public class WaysToChange {
+
+    public static void main(String[] args) {
+        int n = 30;
+        System.out.println(new WaysToChange().waysToChange(n));
+        System.out.println(new WaysToChange().waysToChange_optimise(n));
+    }
+
+    public int waysToChange(int n) {
+        int[] coins = {1, 5, 10, 25};
+        int[][] dp = new int[5][n + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= 4; i++){
+            dp[i][0] = 1;
+            int coin = coins[i - 1];
+            for (int j = 0; j <= n; j++){
+                if (j >= coin)
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coin] % 1000000007 ;
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[4][n] % 1000000007;
+    }
+
+    public int waysToChange_optimise(int n){
+        int[] coins = {1, 5, 10, 25};
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int j = coin; j <= n; j++) {
+                dp[j] = (dp[j] + dp[j - coin]) % 1000000007;
+            }
+        }
+        return dp[n] ;
+    }
+}
