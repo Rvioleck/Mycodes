4b13943e14fbfca475b173b988c99fa1ec716590
iiikkk <352453347@qq.com>
2021-05-14 19:52:53 +0800

    2021-5-14 19:52:47

---
+++ src/LeetCode_JAVA/DynamicProgram/NumWays.java
@@ -0,0 +1,52 @@
+package LeetCode_JAVA.DynamicProgram;
+
+import java.util.Arrays;
+
+public class NumWays {
+
+    public static void main(String[] args) {
+        int steps = 5, arrLen = 20;
+        System.out.println(new NumWays().numWays(steps, arrLen));
+        System.out.println(new NumWays().numWaysOptimise(steps, arrLen));
+    }
+
+    public int numWays(int steps, int arrLen) {
+        int max = Math.min(steps / 2, arrLen - 1);
+        int mod = (int)1e9+7;
+        int[][] dp = new int[steps + 1][max + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= steps; ++i){
+            for (int j = 0; j <= Math.min(max, steps - i); ++j){
+                dp[i][j] = (dp[i][j] + dp[i - 1][j]) % mod;
+                if (j - 1 >= 0){
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - 1]) % mod;
+                }
+                if (j + 1 <= max){
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j + 1]) % mod;
+                }
+            }
+        }
+        return dp[steps][0];
+    }
+
+    public int numWaysOptimise(int steps, int arrLen){
+        int max = Math.min(steps / 2, arrLen - 1);
+        int mod = (int)1e9+7;
+        int[][] dp = new int[2][max + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= steps; ++i){
+            for (int j = 0; j <= Math.min(max, steps - i); ++j){
+                dp[1][j] = dp[0][j] % mod;
+                if (j - 1 >= 0){
+                    dp[1][j] = (dp[1][j] + dp[0][j - 1]) % mod;
+                }
+                if (j + 1 <= max){
+                    dp[1][j] = (dp[1][j] + dp[0][j + 1]) % mod;
+                }
+            }
+            dp[0] = Arrays.copyOf(dp[1], max + 1);
+        }
+        return dp[0][0];
+    }
+
+}
---
+++ src/LeetCode_JAVA/Greedy_Algorithm/IntToRoman.java
@@ -0,0 +1,28 @@
+package LeetCode_JAVA.Greedy_Algorithm;
+
+public class IntToRoman {
+
+    public static void main(String[] args) {
+        int num = 1943;
+        System.out.println(new IntToRoman().intToRoman(num));
+    }
+    
+    public String intToRoman(int num) {
+        String[] roman = new String[]{
+                "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+        };
+        int[] value = new int[]{
+                1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
+        };
+        StringBuilder res = new StringBuilder();
+        int i = 0;
+        while (num != 0){
+            while (num < value[i]){
+                ++i;
+            }
+            num -= value[i];
+            res.append(roman[i]);
+        }
+        return res.toString();
+    }
+}
